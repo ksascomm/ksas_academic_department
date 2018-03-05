4afb3f3449e6714d2d6b874035146e393d80c25e
ksascomm <ksasweb@jhu.edu>
2018-03-05 17:00:03 -0500

    update directory thumb size & style edits

--- library/theme-support.php
+++ library/theme-support.php
@@ -31,7 +31,7 @@ function foundationpress_theme_support() {
 			// Add post thumbnail support: http://codex.wordpress.org/Post_Thumbnails
 			add_theme_support( 'post-thumbnails' );
 			set_post_thumbnail_size( 250, 250, true );   // default thumb size
-			add_image_size( 'directory', 180, 260, true );
+			add_image_size( 'directory', 150, 217, true );
 			add_image_size( 'article', 360, 240, true );
 
 			// RSS thingy
