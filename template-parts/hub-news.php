ff2941cdb4894a548b123f24daddcc463e42f7f9
ksascomm <ksasweb@jhu.edu>
2018-03-05 15:38:15 -0500

    column changes

--- template-parts/hub-news.php
+++ template-parts/hub-news.php
@@ -1,15 +1,14 @@
 <?php
 /**
- * The default template for displaying news via the hub's api
- *
- * @package FoundationPress
- * @since FoundationPress 1.0.0
- */
+* The default template for displaying news via the hub's api
+*
+* @package FoundationPress
+* @since FoundationPress 1.0.0
+*/
 ?>
 <?php
 $theme_option = flagship_sub_get_global_options();
 $selection = $theme_option['flagship_sub_hub_topic_keyword'];
-
 if ('keyword' === $selection ) {
 	$keywords = $theme_option['flagship_sub_hub_keywords'];
 	$hub_url = 'https://api.hub.jhu.edu/articles?v=1&key=bed3238d428c2c710a65d813ebfb2baa664a2fef&tags=' . $keywords . '&per_page=3';
@@ -25,34 +24,34 @@ if ('keyword' === $selection ) {
 	}
 	$hub_articles = $hub_results['_embedded'];
 	foreach ($hub_articles['articles'] as $hub_article ) {
-    ?>
+?>
 <article class="post-listing hub-article" aria-labelledby="post-<?php echo $hub_article['id']; ?>">
 	<header>
-	<?php
-	$date = $hub_article['publish_date'];
-	// $dt = new DateTime("@$date");
-	?>
-	<time class="updated" datetime=""><?php echo date('F d, Y', $date); ?></time>
-	<h2><a href="<?php echo $hub_article['url']; ?>" id="post-<?php echo $hub_article['id']; ?>"><?php echo $hub_article['headline']; ?></a></h2>
+		<?php
+		$date = $hub_article['publish_date'];
+		// $dt = new DateTime("@$date");
+		?>
+		<time class="updated" datetime=""><?php echo date('F d, Y', $date); ?></time>
+		<h2><a href="<?php echo $hub_article['url']; ?>" id="post-<?php echo $hub_article['id']; ?>"><?php echo $hub_article['headline']; ?></a></h2>
 	</header>
 	<div class="entry-content" itemprop="text">
-	<div class="grid-x">
-	<div class="medium-4 large-3 cell">
-	<img class="news-thumb" src="<?php echo $hub_article['_embedded']['image_thumbnail'][0]['sizes']['thumbnail']; ?>" alt="From The Hub: <?php echo $hub_article['headline']; ?>" />
-	</div>
-	<div class="medium-8 large-9 cell">
-	<summary>
-	<p>
-	<?php
-	echo $hub_article['subheadline'];
-	if (empty($hub_article['subheadline']) ) {
-		echo $hub_article['excerpt'];
-		}
-	?>
-	</p>
-	</summary>
-	</div>
-	</div>
+		<div class="grid-x">
+			<div class="small-6 large-4 cell">
+				<img class="news-thumb" src="<?php echo $hub_article['_embedded']['image_thumbnail'][0]['sizes']['thumbnail']; ?>" alt="From The Hub: <?php echo $hub_article['headline']; ?>" />
+			</div>
+			<div class="small-6 large-8 cell">
+				<summary>
+				<p>
+					<?php
+					echo $hub_article['subheadline'];
+					if (empty($hub_article['subheadline']) ) {
+						echo $hub_article['excerpt'];
+						}
+					?>
+				</p>
+				</summary>
+			</div>
+		</div>
 	</div>
 </article>
 <?php } ?>
