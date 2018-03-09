6c1172f28c9a56e070f0be3e57d0a29f3d9c3e79
ksascomm <ksasweb@jhu.edu>
2018-03-09 11:59:16 -0500

    use Hub's Topic id instead of name

--- template-parts/hub-news.php
+++ template-parts/hub-news.php
@@ -14,8 +14,20 @@ if ('keyword' === $selection ) {
 	$hub_url = 'https://api.hub.jhu.edu/articles?v=1&key=bed3238d428c2c710a65d813ebfb2baa664a2fef&tags=' . $keywords . '&per_page=3';
 } elseif ('topic' === $selection ) {
 	$topic = $theme_option['flagship_sub_hub_topic'];
-	$hub_url = 'https://api.hub.jhu.edu/topics/' . $topic . '/articles?v=1&key=bed3238d428c2c710a65d813ebfb2baa664a2fef&per_page=3';
-}
+	$topics = array(
+		'arts-culture' => 32,
+		'at-work' => 3158,
+		'athletics' => 34,
+		'health' => 31,
+		'politics-society' => 167,
+		'science-technology' => 33,
+		'student-life' => 36,
+		'university-news' => 35,
+		'voices-opinion' => 37
+		);
+	$topic_slug = $theme_option['flagship_sub_hub_topic'];
+	$hub_url = 'https://api.hub.jhu.edu/topics/' . $topics[$topic_slug] . '/articles?v=1&key=bed3238d428c2c710a65d813ebfb2baa664a2fef&per_page=3&source=all';
+	}
 	$hub_call = wp_remote_get($hub_url);
 	if (is_array($hub_call) && ! empty($hub_call['body']) ) {
 	$hub_results = json_decode($hub_call['body'], true);
@@ -23,6 +35,7 @@ if ('keyword' === $selection ) {
 	return false; // wp_remote_get failed somehow
 	}
 	$hub_articles = $hub_results['_embedded'];
+	if (is_array($hub_articles)) {
 	foreach ($hub_articles['articles'] as $hub_article ) {
 ?>
 <article class="post-listing hub-article" aria-labelledby="post-<?php echo $hub_article['id']; ?>">
@@ -54,4 +67,4 @@ if ('keyword' === $selection ) {
 		</div>
 	</div>
 </article>
-<?php } ?>
+<?php } } ?>
