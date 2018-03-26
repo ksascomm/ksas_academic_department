9a43e2633b9abf0093c124e1e937994ffa329da6
ksascomm <ksasweb@jhu.edu>
2018-03-26 10:29:23 -0400

    code cleanup

--- library/foundation.php
+++ library/foundation.php
@@ -8,7 +8,7 @@
 
 // Pagination.
 if ( ! function_exists( 'foundationpress_pagination' ) ) :
-function foundationpress_pagination() {
+	function foundationpress_pagination() {
 		global $wp_query;
 
 		$big = 999999999; // This needs to be an unlikely integer
@@ -16,30 +16,30 @@ function foundationpress_pagination() {
 		// For more options and info view the docs for paginate_links()
 		// http://codex.wordpress.org/Function_Reference/paginate_links
 		$paginate_links = paginate_links(
-         array(
-			 'base' => str_replace( $big, '%#%', html_entity_decode( get_pagenum_link( $big ) ) ),
-			 'current' => max( 1, get_query_var( 'paged' ) ),
-			 'total' => $wp_query->max_num_pages,
-			 'mid_size' => 5,
-			 'prev_next' => true,
-			 'prev_text' => __( '&laquo;', 'foundationpress' ),
-			 'next_text' => __( '&raquo;', 'foundationpress' ),
-			 'type' => 'list',
-		 )
-			);
+			array(
+				'base'      => str_replace( $big, '%#%', html_entity_decode( get_pagenum_link( $big ) ) ),
+				'current'   => max( 1, get_query_var( 'paged' ) ),
+				'total'     => $wp_query->max_num_pages,
+				'mid_size'  => 5,
+				'prev_next' => true,
+				'prev_text' => __( '&laquo;', 'foundationpress' ),
+				'next_text' => __( '&raquo;', 'foundationpress' ),
+				'type'      => 'list',
+			)
+		);
 
-			$paginate_links = str_replace( "<ul class='page-numbers'>", "<ul class='pagination text-center' role='navigation' aria-label='Pagination'>", $paginate_links );
-			$paginate_links = str_replace( '<li><span class="page-numbers dots">', "<li><a href='#'>", $paginate_links );
-			$paginate_links = str_replace( '</span>', '</a>', $paginate_links );
-			$paginate_links = str_replace( "<li><span class='page-numbers current'>", "<li class='current'>", $paginate_links );
-			$paginate_links = str_replace( "<li><a href='#'>&hellip;</a></li>", "<li><span class='dots'>&hellip;</span></li>", $paginate_links );
-			$paginate_links = preg_replace( '/\s*page-numbers/', '', $paginate_links );
+		$paginate_links = str_replace( "<ul class='page-numbers'>", "<ul class='pagination text-center' role='navigation' aria-label='Pagination'>", $paginate_links );
+		$paginate_links = str_replace( '<li><span class="page-numbers dots">', "<li><a href='#'>", $paginate_links );
+		$paginate_links = str_replace( '</span>', '</a>', $paginate_links );
+		$paginate_links = str_replace( "<li><span class='page-numbers current'>", "<li class='current'>", $paginate_links );
+		$paginate_links = str_replace( "<li><a href='#'>&hellip;</a></li>", "<li><span class='dots'>&hellip;</span></li>", $paginate_links );
+		$paginate_links = preg_replace( '/\s*page-numbers/', '', $paginate_links );
 
-			// Display the pagination if more than one page is found.
-			if ( $paginate_links ) {
-				echo $paginate_links;
-				}
-}
+		// Display the pagination if more than one page is found.
+		if ( $paginate_links ) {
+			echo $paginate_links;
+		}
+	}
 endif;
 
 /**
