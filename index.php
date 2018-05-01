45b1b021f331b05c54fba728fa4c59304e53931f
ksascomm <ksasweb@jhu.edu>
2018-05-01 16:25:53 -0400

    hide faculty books conditional on index.php

--- index.php
+++ index.php
@@ -15,37 +15,46 @@
 
 get_header();
 	$theme_option = flagship_sub_get_global_options();
-	$collection_name = $theme_option['flagship_sub_search_collection']; ?>
+	$collection_name = $theme_option['flagship_sub_search_collection']; 
+	$news_query_cond = $theme_option['flagship_sub_news_query_cond']; ?>
 	<div class="main-container" id="page">
 		<div class="main-grid">
 			<main class="main-content">
 				<h1 class="page-title"><?php echo $theme_option['flagship_sub_feed_name']; ?> Archive</h1>
-			<?php if ( have_posts() ) : ?>
+			
+			<?php $paged = (get_query_var('paged')) ? (int) get_query_var('paged') : 1;
 
-				<?php /* Start the Loop */ ?>
-				<?php
-                while ( have_posts() ) :
-the_post();
-?>
+				if ($news_query_cond === 1) {
+					$news_archive_query = new WP_Query(array(
+						'post_type' => 'post',
+						'tax_query' => array(
+							array(
+								'taxonomy' => 'category',
+								'field' => 'slug',
+								'terms' => array( 'books' ),
+								'operator' => 'NOT IN'
+							)
+						),
+						'posts_per_page' => 10,
+						'paged' => $paged));
+				} else {
+					$news_archive_query = new WP_Query(array(
+						'post_type' => 'post',
+						'posts_per_page' => 10,
+						'paged' => $paged
+						)); 
+				}
+
+			while ($news_archive_query->have_posts()) : $news_archive_query->the_post(); ?>
+				
 					<?php get_template_part( 'template-parts/content-news', get_post_format() ); ?>
 				<?php endwhile; ?>
 
-				<?php else : ?>
-					<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
-				<?php endif; // End have_posts() check. ?>
 
 				<?php /* Display navigation to next/previous pages when applicable */ ?>
-				<?php
-				if ( function_exists( 'foundationpress_pagination' ) ) :
-					foundationpress_pagination();
-				elseif ( is_paged() ) :
-				?>
-					<nav id="post-nav">
-						<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'foundationpress' ) ); ?></div>
-						<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'foundationpress' ) ); ?></div>
-					</nav>
-			<?php endif; ?>
+				<div class="news-post-pagination text-center">
+					<?php flagship_pagination($news_archive_query->max_num_pages); ?>
+				</div>
 
 			</main>
 			<?php get_sidebar(); ?>
