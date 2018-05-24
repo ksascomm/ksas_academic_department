d3cf36ca618c8d984967649292c4ad99332ce9cc
ksascomm <ksasweb@jhu.edu>
2018-05-24 09:31:26 -0400

    new page template: no featured image

---
+++ page-templates/page-no-featured-image.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Template Name: No Featured Image
+*/
+get_header(); ?>
+
+<div class="main-container" id="page">
+    <div class="main-grid">
+        <main class="main-content">
+            <?php while ( have_posts() ) : the_post(); ?>
+                <?php get_template_part( 'template-parts/content', 'page' ); ?>
+            <?php endwhile; ?>
+            <?php if (is_page('Sitemap') ) : ?>
+            	<?php
+                wp_nav_menu(
+                     array(
+						 'theme_location' => 'top-bar-r',
+						 'menu_class' => 'vertical',
+                         'items_wrap' => '<ul class="%2$s" role="navigation" aria-label="Sitemap Menu">%3$s</ul>',
+					 )
+                    );
+                ?>
+            <?php endif; ?>
+        </main>
+        <?php get_sidebar(); ?>
+    </div>
+</div>
+<?php
+get_footer();
