aacd3c4abc32d83f803fa586bb8af2c36e7a8141
ksascomm <ksasweb@jhu.edu>
2018-05-24 14:37:56 -0400

    removed repetitive 'get_terms' functions; exclude specific roles by slugs

--- page-templates/people-directory.php
+++ page-templates/people-directory.php
@@ -5,11 +5,22 @@ Template Name: People Directory
 get_header(); ?>
 <?php
 	$theme_option = flagship_sub_get_global_options();
+	$ids_to_exclude = array();
+	$roles_to_exclude =  get_terms(
+		'role', array(
+	    'fields' => 'ids',
+	    'slug' => array( 'graduate', 'job-market-candidate', 'graduate-student','research' ),
+	));
+	//convert the role slug to corresponding IDs
+	if( !is_wp_error( $roles_to_exclude ) && count($roles_to_exclude) > 0){
+	    $ids_to_exclude = $roles_to_exclude; 
+	}
 	$roles = get_terms(
         'role', array(
 			'orderby'       => 'ID',
 			'order'         => 'ASC',
 			'hide_empty'    => true,
+			'exclude'       => $ids_to_exclude,
 		)
         );
 	$filters = get_terms(
@@ -53,14 +64,7 @@ the_post();
 					<?php //ROLE FILTER BUTTONS
 					 if ($theme_option['flagship_sub_role_search'] == true ) : ?>
 					<?php
-	                $roles = get_terms(
-	                    'role', array(
-							'orderby'       => 'ID',
-							'order'         => 'ASC',
-							'hide_empty'    => true,
-							'exclude'       => array('58', '63', '833'), // job-market-candidate
-						)
-	                    );
+	           	
 						$count_roles = count($roles);
 					if ( $count_roles > 0 ) :
 	                ?>
@@ -93,14 +97,6 @@ the_post();
 					if ($theme_option['flagship_sub_research_search'] == true ) : ?>
 					<p>Filter all by Research Area:</p>
 					<?php
-	                $filters = get_terms(
-	                    'filter', array(
-							'orderby'       => 'name',
-							'order'         => 'ASC',
-							'hide_empty'    => true,
-						)
-	                    );
-
 						$count_filters = count($filters);
 					if ( $count_filters > 0 ) :
 	                ?>
@@ -118,38 +114,30 @@ the_post();
 				<ul class="directory">
 					<?php
                     foreach ($roles as $role ) {
-					$role_slug = $role->slug;
-					$role_name = $role->name;
-					if ($role_slug !== 'graduate' && $role_slug !== 'job-market-candidate' && $role_slug !== 'graduate-student' && $role_slug !== 'research' ) {
-							$people_query = new WP_Query(
-                            array(
-								'post_type' => 'people',
-								'role' => $role_slug,
-								'meta_key' => 'ecpt_people_alpha',
-								'orderby' => 'meta_value',
-								'order' => 'ASC',
-								'posts_per_page' => '250',
-							)
-								);
-											}
-					if ($people_query->have_posts() ) :
-							?>
+						$role_slug = $role->slug;
+						$role_name = $role->name;
+				
+						$people_query = new WP_Query(
+	                    array(
+							'post_type' => 'people',
+							'role' => $role_slug,
+							'meta_key' => 'ecpt_people_alpha',
+							'orderby' => 'meta_value',
+							'order' => 'ASC',
+							'posts_per_page' => '250',
+						) );
+						
+						if ($people_query->have_posts() ) : ?>
 							<li class="person sub-head quicksearch-match <?php echo $role->slug; ?>">
-							<h2 class="black capitalize"><?php echo $role_name; ?>
-							</h2>
+								<h2 class="black capitalize"><?php echo $role_name; ?> </h2>
 							</li>
-							<?php
-							while ($people_query->have_posts() ) :
-	$people_query->the_post();
-	?>
-					
-					<?php
-                    if ( get_post_meta($post->ID, 'ecpt_bio', true) ) :
-							get_template_part( 'template-parts/hasbio-loop' );
-						else :
-							get_template_part( 'template-parts/nobio-loop' );
-						endif;
-						?>
+							<?php while ($people_query->have_posts() ) : $people_query->the_post(); ?>
+								<?php
+			                    if ( get_post_meta($post->ID, 'ecpt_bio', true) ) :
+									get_template_part( 'template-parts/hasbio-loop' );
+								else :
+									get_template_part( 'template-parts/nobio-loop' );
+								endif; ?>
 						<?php endwhile; endif; } wp_reset_postdata(); ?>
 					<li id="noResult">
 						<div class="callout warning">
