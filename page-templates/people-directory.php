78a4571a0eacad6178eb9823c8e972f8540f8dbb
ksascomm <ksasweb@jhu.edu>
2018-05-24 12:04:37 -0400

    fix bug of faculty ID hidden from filter

--- page-templates/people-directory.php
+++ page-templates/people-directory.php
@@ -58,7 +58,7 @@ the_post();
 							'orderby'       => 'ID',
 							'order'         => 'ASC',
 							'hide_empty'    => true,
-							'exclude'       => array('63', '58', '61'), // job-market-candidate
+							'exclude'       => array('58', '63', '833'), // job-market-candidate
 						)
 	                    );
 						$count_roles = count($roles);
