0893542ae237f0ae640bcec085106209a49dd40b
ksascomm <ksasweb@jhu.edu>
2018-06-01 10:13:14 -0400

    correct Krieger typo

--- search.php
+++ search.php
@@ -48,7 +48,7 @@ the_post();
 					<h5 class="white">Search Krieger Network?</h5>
 				</div>
 				<div class="textwidget">
-					<p>You are currently searching on <strong><?php echo bloginfo('name'); ?></strong>. Would you like to search the entire <a href="http://krieger.jhu.edu/search/?q=<?php echo esc_attr(get_search_query()); ?>">Kreiger network?</a></p>
+					<p>You are currently searching on <strong><?php echo bloginfo('name'); ?></strong>. Would you like to search the entire <a href="http://krieger.jhu.edu/search/?q=<?php echo esc_attr(get_search_query()); ?>">Krieger network?</a></p>
 				</div>
 			</div>
 		</aside>
