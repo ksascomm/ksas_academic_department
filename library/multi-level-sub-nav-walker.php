e8a553d64fbf19fb2a657ab471d477d5fd993c98
ksascomm <ksasweb@jhu.edu>
2018-08-07 13:17:17 -0400

    restore file

--- library/multi-level-sub-nav-walker.php
+++ library/multi-level-sub-nav-walker.php
@@ -1,12 +1,10 @@
-<?php 
-// SIDEBAR NAV MENU
-
-// add hook
+<?php // add hook
 add_filter( 'wp_nav_menu_objects', 'my_wp_nav_menu_objects_sub_menu', 10, 2 );
 
 // filter_hook function to react on sub_menu flag
 function my_wp_nav_menu_objects_sub_menu( $sorted_menu_items, $args ) {
   if ( isset( $args->sub_menu ) ) {
+  		$current_id = 0;
 		$root_id = 0;
 
 		// find the current menu item
@@ -26,7 +24,8 @@ function my_wp_nav_menu_objects_sub_menu( $sorted_menu_items, $args ) {
 					if ( $menu_item->ID == $prev_root_id ) {
 						$prev_root_id = $menu_item->menu_item_parent;
 						// don't set the root_id to 0 if we've reached the top of the menu
-						if ( $prev_root_id != 0 ) { $root_id = $menu_item->menu_item_parent;
+						if ( $prev_root_id != 0 ) {
+$root_id = $menu_item->menu_item_parent;
                       }
 						break;
 					}
@@ -37,7 +36,8 @@ function my_wp_nav_menu_objects_sub_menu( $sorted_menu_items, $args ) {
 		$menu_item_parents = array();
 		foreach ( $sorted_menu_items as $key => $item ) {
 			// init menu_item_parents
-			if ( $item->ID == $root_id ) { $menu_item_parents[] = $item->ID;
+			if ( $item->ID == $root_id ) {
+$menu_item_parents[] = $item->ID;
           }
 
 			if ( in_array( $item->menu_item_parent, $menu_item_parents ) ) {
@@ -53,4 +53,54 @@ function my_wp_nav_menu_objects_sub_menu( $sorted_menu_items, $args ) {
   } else {
 		return $sorted_menu_items;
   }// End if().
+}
+
+
+// add hook
+add_filter( 'wp_nav_menu_objects', 'my_wp_nav_menu_objects_sub_menu_children', 10, 2 );
+// filter_hook function to react on sub_menu flag
+function my_wp_nav_menu_objects_sub_menu_children( $sorted_menu_items, $args ) {
+  if ( isset( $args->sub_menu_children ) ) {
+		$current_id = 0;
+		$root_id = 0;
+
+		// find the current menu item
+		foreach ( $sorted_menu_items as $menu_item ) {
+			if ( $menu_item->current ) {
+				// store the current menu item
+				$current_id = $menu_item->ID;
+				// set the root id based on whether the current menu item has a parent or not
+				$root_id = ( $menu_item->menu_item_parent ) ? $menu_item->menu_item_parent : $menu_item->ID;
+				break;
+			}
+			}
+
+		// find the current menu items children
+		foreach ( $sorted_menu_items as $menu_item ) {
+			if ( $menu_item->menu_item_parent == $current_id ) {
+				// it has children, make sure the root id is current menu item
+				$root_id = $current_id;
+				break;
+			}
+			}
+
+		$menu_item_parents = array();
+		foreach ( $sorted_menu_items as $key => $item ) {
+			// init menu_item_parents
+			if ( $item->ID == $root_id ) {
+$menu_item_parents[] = $item->ID;
+		}
+			if ( in_array( $item->menu_item_parent, $menu_item_parents ) ) {
+				// part of sub-tree: keep!
+				$menu_item_parents[] = $item->ID;
+			} else if ( ! ( isset( $args->show_parent ) && in_array( $item->ID, $menu_item_parents ) ) ) {
+				// not part of sub-tree: away with it!
+				unset( $sorted_menu_items[ $key ] );
+			}
+			}
+
+		return $sorted_menu_items;
+  } else {
+		return $sorted_menu_items;
+  }
 }
