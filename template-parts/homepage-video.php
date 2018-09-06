6da8382c3d0b83236868b8a8f2b5bfd7ff757b2a
ksascomm <ksasweb@jhu.edu>
2018-09-06 10:57:34 -0400

    remove unused code

--- template-parts/homepage-video.php
+++ template-parts/homepage-video.php
@@ -7,9 +7,6 @@
 	        <source src="<?php the_field( 'source_ogg' ); ?>" type="video/ogg">
 	    </video>
 		<div class="controls">
-			<!--<button id="playback-button" class="toggle-playback" onclick="playPause()">
-				<span class="fas fa-pause-circle"></span>
-			</button>-->
 			<button class="caption" data-tooltip tabindex="1" title="<?php the_field( 'video_caption_summary' ); ?>">
 				<span class="fas fa-info-circle"></span>
 			</button>
@@ -19,27 +16,3 @@
 		</div>    
 	</div>
 </div>
-
-<!--<script defer>
-	var video = document.getElementById("homepage-hero-video");
-	var isPlaying = false;
-
-	// Get the button
-	var btn = document.getElementById("playback-button");
-
-	// Pause and play the video, and change the button text
-	function playPause() {
-	    if (video.paused) {
-	        video.play();
-	        btn.innerHTML = "<span class='fas fa-pause-circle'></span>";
-	    } else {
-	        video.pause();
-	        btn.innerHTML = "<span class='fas fa-play-circle'></span>";
-	    }
-	    isPlaying = !isPlaying;
-}
-
-  //$( ".toggle-playback" ).click(function() {
-    //$( ".toggle-playback" ).css('background', 'rgba(255,255,255, 0.3)');
-  //});
-</script>-->
