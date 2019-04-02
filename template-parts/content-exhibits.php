ae05a404cbcafce967dc4c2d54145be3f051c027
ksascomm <ksasweb@jhu.edu>
2019-04-02 11:42:06 -0400

    add modal for featured images

--- template-parts/content-exhibits.php
+++ template-parts/content-exhibits.php
@@ -1,45 +1,50 @@
 <?php
 /**
- * The default template for displaying exhibits content
- *
- * Used for both single exhibits.
- *
- * @package FoundationPress
- * @since FoundationPress 1.0.0
- */
-
+* The default template for displaying exhibits content
+*
+* Used for both single exhibits.
+*
+* @package FoundationPress
+* @since FoundationPress 1.0.0
+*/
 ?>
-
 <article id="post-<?php the_ID(); ?>" <?php post_class('post-listing exhibit'); ?>>
 	<header>
 		<h1>
-			<?php the_title(); ?>
+		<?php the_title(); ?>
 		</h1>
 	</header>
-
 	<div class="entry-content">
-		<div class="exhibit-body">						
-			<?php
-            the_post_thumbnail(
-                'featured-large',
+		<div class="exhibit-body">
+			<a data-open="modal">
+			<?php the_post_thumbnail( 'featured-medium',
 				array(
+					'class' => 'thumbnail',
 					'alt' => trim(strip_tags( $post->post_title )),
 					'aria-label' => 'Click on the featured image to expand it',
-				)
-                );
-                ?>
-          	<h3>
+					)
+				);
+				?>
+			</a>
+			<h3>
 			<?php if (get_post_meta($post->ID, 'ecpt_location', true) ) : ?>
-				Location: <small><?php echo get_post_meta($post->ID, 'ecpt_location', true); ?></small><br>
+			Location: <small><?php echo get_post_meta($post->ID, 'ecpt_location', true); ?></small><br>
 			<?php endif; ?>
 			<?php if (get_post_meta($post->ID, 'ecpt_dates', true) ) : ?>
-				Dates: <small><?php echo get_post_meta($post->ID, 'ecpt_dates', true); ?></small>
+			Dates: <small><?php echo get_post_meta($post->ID, 'ecpt_dates', true); ?></small>
 			<?php endif; ?>
 			</h3>
 			<?php if (get_post_meta($post->ID, 'ecpt_description_full', true) ) : ?>
-				<h3>Description</h3>
-				<?php echo get_post_meta($post->ID, 'ecpt_description_full', true); ?>
+			<h3>Description</h3>
+			<?php echo get_post_meta($post->ID, 'ecpt_description_full', true); ?>
 			<?php endif; ?>
 		</div>
 	</div>
 </article>
+<div id="modal" class="small reveal" data-reveal data-animation-in="scale-in-up" data-animation-out="scale-out-down">
+	<?php the_post_thumbnail( 'full');?>
+	<p><?php the_post_thumbnail_caption();?></p>
+	<button class="close-button" data-close aria-label="Close modal" type="button">
+		<span aria-hidden="true">&times;</span>
+	</button>
+ </div>
