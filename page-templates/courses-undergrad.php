10b8d5a09c799f3aec4d5357bc590f587ff2a914
ksascomm <ksasweb@jhu.edu>
2019-04-02 11:50:37 -0400

    summer and fall 2019 courses added

--- page-templates/courses-undergrad.php
+++ page-templates/courses-undergrad.php
@@ -12,9 +12,9 @@ get_header(); ?>
 		$department_unclean = $theme_option['flagship_sub_isis_name'];
 		$department = str_replace(' ', '%20', $department_unclean);
 		$department = str_replace('&', '%26', $department);
-		//$fall = 'fall%202018';
+		$fall = 'fall%202019';
 		$spring = 'spring%202019';
-		//$summer = 'summer%202018';
+		$summer = 'summer%202019';
 		$open = 'open';
 		$approval = 'approval%20required';
 		$closed = 'closed';
@@ -31,18 +31,18 @@ get_header(); ?>
         );
 
 	// Create API Url calls
-		//$courses_fall_url = 'https://sis.jhu.edu/api/classes?key=' . $key . '&School=Krieger%20School%20of%20Arts%20and%20Sciences&Term=' . $fall . '&Department=AS%20' . $department;
+		$courses_fall_url = 'https://sis.jhu.edu/api/classes?key=' . $key . '&School=Krieger%20School%20of%20Arts%20and%20Sciences&Term=' . $fall . '&Department=AS%20' . $department;
 		$courses_spring_url = 'https://sis.jhu.edu/api/classes?key=' . $key . '&School=Krieger%20School%20of%20Arts%20and%20Sciences&Term=' . $spring . '&Department=AS%20' . $department;
 		// $courses_intersession_url = 'https://sis.jhu.edu/api/classes?key=' . $key . '&School=Krieger%20School%20of%20Arts%20and%20Sciences&Term=' . $intersession . '&Department=AS%20' . $department;
-		//$courses_summer_url = 'https://sis.jhu.edu/api/classes?key=' . $key . '&School=Krieger%20School%20of%20Arts%20and%20Sciences&Term=' . $summer . '&Department=AS%20' . $department;
+		$courses_summer_url = 'https://sis.jhu.edu/api/classes?key=' . $key . '&School=Krieger%20School%20of%20Arts%20and%20Sciences&Term=' . $summer . '&Department=AS%20' . $department;
 		$courses_call = array(
-			//$courses_fall_url,
+			$courses_fall_url,
 			$courses_spring_url,
-			//$courses_summer_url,
+			$courses_summer_url,
 		);
-		//$courses_call_fall = $courses_fall_url;
+		$courses_call_fall = $courses_fall_url;
 		$courses_call_spring = $courses_spring_url;
-		//$courses_call_summer = $courses_summer_url;
+		$courses_call_summer = $courses_summer_url;
 
 	// Course display callback function
 		function display_courses( $result ) {
@@ -130,6 +130,8 @@ get_header(); ?>
             <?php endwhile; ?>    
 			<ul class="tabs" data-tabs id="courses-tabs">
 			 	<li class="tabs-title is-active"><a href="#Spring">Spring 2019</a></li>
+			 	<li class="tabs-title"><a href="#Summer">Summer 2019</a></li>
+			 	<li class="tabs-title"><a href="#Fall">Fall 2019</a></li>
 			</ul>
 			<div class="tabs-content course-listings" data-tabs-content="courses-tabs">
 				 <div class="tabs-panel is-active" id="Spring">
@@ -150,7 +152,45 @@ get_header(); ?>
 							<?php $course_curl->get($courses_call_spring, 'parse_courses'); ?>
 						</tbody>
 					</table>
-				 </div>				 
+				 </div>
+				<div class="tabs-panel" id="Summer">
+				 	<p class="show-for-sr" id="tblDescSummer">Column one has the course number and section. Other columns show the course title, days offered, instructor's name, room number, if the course is cross-referenced with another program, and a option to view additional course information in a pop-up window.</p>
+				 	<table aria-describedby="tblDescSummer" class="course-table">
+						<thead>
+							<tr>
+								<th>Course # (Section)</th>
+								<th>Title</th>
+								<th>Day/Times</th>
+								<th>Instructor</th>
+								<th>Room</th>
+								<th>PosTag(s)</th>
+								<th>Info</th>
+							</tr>
+						</thead>
+						<tbody>
+							<?php $course_curl->get($courses_call_summer, 'parse_courses'); ?>
+						</tbody>
+					</table>
+				</div>					 
+				<div class="tabs-panel" id="Fall">
+				 	<p class="show-for-sr" id="tblDescFall">Column one has the course number and section. Other columns show the course title, days offered, instructor's name, room number, if the course is cross-referenced with another program, and a option to view additional course information in a pop-up window.</p>
+				 	<table aria-describedby="tblDescFall" class="course-table">
+						<thead>
+							<tr>
+								<th>Course # (Section)</th>
+								<th>Title</th>
+								<th>Day/Times</th>
+								<th>Instructor</th>
+								<th>Room</th>
+								<th>PosTag(s)</th>
+								<th>Info</th>
+							</tr>
+						</thead>
+						<tbody>
+							<?php $course_curl->get($courses_call_fall, 'parse_courses'); ?>
+						</tbody>
+					</table>
+				</div>								 
 			</div>
         </main>
         <?php get_sidebar(); ?>
