1b73668a13edf4700709566bb613f435f2d177da
ksascomm <ksasweb@jhu.edu>
2019-06-13 15:31:02 -0400

    rework pagination

--- index.php
+++ index.php
@@ -52,10 +52,16 @@ get_header();
 
 
 				<?php /* Display navigation to next/previous pages when applicable */ ?>
-				<div class="news-post-pagination text-center">
-					<?php flagship_pagination($news_archive_query->max_num_pages); ?>
-				</div>
-
+			<?php
+			if ( function_exists( 'foundationpress_pagination' ) ) :
+				foundationpress_pagination();
+			elseif ( is_paged() ) :
+			?>
+				<nav id="post-nav">
+					<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'foundationpress' ) ); ?></div>
+					<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'foundationpress' ) ); ?></div>
+				</nav>
+			<?php endif; ?>
 			</main>
 			<?php get_sidebar(); ?>
 		</div>
