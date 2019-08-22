fba175ebf77c5e969e5af1b69d7292178de431de
ksascomm <ksasweb@jhu.edu>
2019-08-22 09:40:56 -0400

    mobile fixes & use pre-existing templates

--- page-templates/front-aghi.php
+++ page-templates/front-aghi.php
@@ -4,73 +4,13 @@ Template Name: Front (AGHI Only)
 */
 get_header(); ?>
 
-<?php
-	$theme_option = flagship_sub_get_global_options();
-	$slider_query = new WP_Query(
-        array(
-			'post_type' => 'slider',
-			'posts_per_page' => 5,
-			'orderby' => 'rand',
-		)
-        );
-	if ( $slider_query->have_posts() ) :
-    ?>
-     
-	<div class="fullscreen-image-slider hide-for-small-only">
-		<div class="orbit" role="region" aria-label="Homepage Slider" data-orbit data-options="animInFromLeft:fade-in; animInFromRight:fade-in; animOutToLeft:fade-out; animOutToRight:fade-out;">
-			<div class="orbit-wrapper">	
-				<?php if ($slider_query->post_count > 1 ) : ?>
-				<div class="orbit-controls">
-					<button class="orbit-previous show-for-large"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button>
-					<button class="orbit-next show-for-large"><span class="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button>
-				</div>
-				<?php endif; ?>
-
-				<ul class="orbit-container">
-				<?php
-                while ($slider_query->have_posts() ) :
-$slider_query->the_post();
-?>
-					<li class="orbit-slide">	
-						<img class="orbit-image hide-for-print" src="<?php echo get_post_meta($post->ID, 'ecpt_slideimage', true); ?>" alt="<?php the_title(); ?>">
-						<?php if (get_the_title() || !empty( get_the_content() )) :?>
-					    <figcaption class="orbit-caption" aria-hidden="true">
-					    	<?php if (get_the_title()) :?>
-					      		<h1><?php the_title(); ?></h1>
-					  		<?php endif;?>
-					  	<?php if ( !empty( get_the_content() ) ):?>
-					      <p><?php echo get_the_content(); ?></p>
-						   <?php if (get_post_meta($post->ID, 'ecpt_button', true) ) : ?>
-								<a href="<?php echo get_post_meta($post->ID, 'ecpt_urldestination', true); ?>" onclick="ga('send', 'event', 'Homepage Slider', 'Click', '<?php echo get_post_meta($post->ID, 'ecpt_urldestination', true); ?>')" aria-label="post-<?php the_ID(); ?>" class="button show-for-large">Find Out More <span class="far fa-arrow-alt-circle-right"></span></a>
-							<?php endif; ?>
-						<?php endif;?>
-					    </figcaption>
-					<?php endif;?>
-			   		</li>
-				<?php endwhile; ?>
-				</ul>	
-			</div>		
-		</div>
-	</div>
+<?php if ( has_post_thumbnail( $post->ID ) ) : ?>
+		<header class="featured-hero homepage hide-for-print" role="banner" data-interchange="[<?php the_post_thumbnail_url('featured-small'); ?>, small], [<?php the_post_thumbnail_url('featured-medium'); ?>, medium], [<?php the_post_thumbnail_url('featured-large'); ?>, large], [<?php the_post_thumbnail_url('featured-xlarge'); ?>, xlarge]" aria-label="Featured Image">
+		</header>
 	<?php endif; ?>
-	<?php
-    $slider_mobile_query = new WP_Query(
-        array(
-			'post_type' => 'slider',
-			'posts_per_page' => 1,
-			'orderby' => 'rand',
-		)
-        );
-		if ( $slider_mobile_query->have_posts() ) : while ($slider_mobile_query->have_posts() ) : $slider_mobile_query->the_post();
-				?>
-				<div class="front-hero-featured-image show-for-small-only hide-for-print" role="banner" aria-label="Mobile Hero Image">
-					<img class="featured-small" src="<?php echo get_post_meta($post->ID, 'ecpt_slideimage', true); ?>" alt="<?php the_title(); ?>">
-				</div>
-		<?php endwhile; endif; ?>
-
-	<?php do_action( 'foundationpress_before_content' ); ?>
+	<?php get_template_part( 'template-parts/homepage-slider' ); ?>
 	<?php while ( have_posts() ) : the_post(); ?>
-		<section class="background-bluejaysblue" id="page" role="main" tabindex="0" aria-label="Website Introduction">
+		<main class="background-bluejaysblue" id="page" tabindex="0" aria-label="Website Introduction">
 			<div class="intro">
 				<div <?php post_class('seo-intro'); ?> id="post-<?php the_ID(); ?>">
 					<?php do_action( 'foundationpress_page_before_entry_content' ); ?>
@@ -79,19 +19,23 @@ $slider_query->the_post();
 					</div>
 				</div>
 			</div>
-		</section>
+		</main>
 	<?php endwhile; ?>
-	<div class="grid-x aghi-events" aria-label="Upcoming Events">
-		<div class="small-12 large-10 large-offset-1 cell events-feed" role="region" aria-label="Events">
-			<div class="aghi-events-intro">
-			   <h1>Upcoming AGHI-sponsored Events</h1>
-				<div class="grid-x">
-					<div class="small-12 columns">
-						<p>A complete calendar of all humanities-related events, including all AGHI-sponsored events, happening at Johns Hopkins is available on our <a href="<?php echo site_url();?>/events/">events page</a>.</p>	
+	<div class="aghi-events">
+		<div class="grid-container">
+			<div class="grid-x" aria-label="Upcoming Events">
+				<div class="small-12 large-10 large-offset-1 cell events-feed" role="region" aria-label="Events">
+					<div class="aghi-events-intro">
+					   <h1>Upcoming AGHI-sponsored Events</h1>
+						<div class="grid-x">
+							<div class="small-12 columns">
+								<p>A complete calendar of all humanities-related events, including all AGHI-sponsored events, happening at Johns Hopkins is available on our <a href="<?php echo site_url();?>/events/">events page</a>.</p>	
+							</div>
+						</div>
+						<?php echo do_shortcode('[ai1ec view="agenda" cat_name="AGHI" events_limit="4"]');	?>
 					</div>
 				</div>
-				<?php echo do_shortcode('[ai1ec view="agenda" cat_name="AGHI" events_limit="4"]');	?>
-			</div>
+			</div>	
 		</div>
 	</div>
 	<?php do_action( 'foundationpress_after_content' ); ?>
