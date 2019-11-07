c1e901e317c74b97a2f9427bc4f9d71e755f58f5
ksascomm <ksasweb@jhu.edu>
2019-11-07 12:10:59 -0500

    Update A&S Magazine url

--- template-parts/roof.php
+++ template-parts/roof.php
@@ -10,7 +10,7 @@
 <ul class="menu simple roof-menu align-right">
 	<li class="roof-padding"><a href="https://www.jhu.edu/admissions/visit/" target="_blank" rel="noopener">Visit</a></li>
 	<li class="bar"></li>
-	<li class="roof-padding"><a href="https://krieger.jhu.edu/magazine/" target="_blank" rel="noopener">A&S Magazine</a></li>
+	<li class="roof-padding"><a href="https://magazine.krieger.jhu.edu/" target="_blank" rel="noopener">A&S Magazine</a></li>
 	<li><a class="button" href="#" aria-label="Explore KSAS" data-toggle="offCanvasTop1">Explore JHU <span class="fas fa-bars" aria-hidden="true"></span></a></li>
 </ul>
 
