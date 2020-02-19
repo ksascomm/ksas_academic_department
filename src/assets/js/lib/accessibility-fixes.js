219a69fdecbb426fe35ab374d6c288f163a755f5
ksascomm <ksasweb@jhu.edu>
2020-02-19 09:51:15 -0500

    Accessibility Fix: remove role=tree item on quicklinks

--- src/assets/js/lib/accessibility-fixes.js
+++ src/assets/js/lib/accessibility-fixes.js
@@ -2,4 +2,5 @@ jQuery(document).ready( function($) {
 	$('ul.sub-menu.submenu.is-drilldown-submenu.invisible').attr('aria-label', 'Quicklinks');
 	$('.mobile-off-canvas-menu ul li').removeAttr('role', 'treeitem');
 	$('.mobile-off-canvas-menu ul li').attr('role', 'menuitem');
+	$('.quicklinks ul li').removeAttr('role', 'treeitem');
 });
