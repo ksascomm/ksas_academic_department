ff2171841efa6fbc06f733615a961403199e0dbd
ksascomm <ksasweb@jhu.edu>
2020-02-19 09:40:58 -0500

    Accessibility fix: Missing required WAI-ARIA states or properties
    Change "treeitem" to "menuitem"

--- src/assets/js/lib/accessibility-fixes.js
+++ src/assets/js/lib/accessibility-fixes.js
@@ -1,3 +1,5 @@
 jQuery(document).ready( function($) {
 	$('ul.sub-menu.submenu.is-drilldown-submenu.invisible').attr('aria-label', 'Quicklinks');
+	$('.mobile-off-canvas-menu ul li').removeAttr('role', 'treeitem');
+	$('.mobile-off-canvas-menu ul li').attr('role', 'menuitem');
 });
