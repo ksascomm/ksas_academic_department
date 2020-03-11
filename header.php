83854901853b22d27ab094f22df1388c3fc456c8
ksascomm <ksasweb@jhu.edu>
2020-03-11 09:59:22 -0400

    Add Alert Bar

--- header.php
+++ header.php
@@ -51,6 +51,9 @@
 		
 	</head>
 	<body <?php body_class(); ?>>
+	<div class="alert" role="navigation" aria-label="COVID-19 Alerts">
+    	<a class="alert-message" href="https://hub.jhu.edu/novel-coronavirus-information/">Suspension of classes and other COVID-19 updates</a>
+	</div>
 	<div role="navigation" aria-label="Skip to main content">
 		<a class="skip-main show-on-focus" href="#page" >Skip to main content</a>
 	</div>
