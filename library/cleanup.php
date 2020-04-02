01c444f7d965fe13b7abf7ebdf88fa20a1127879
ksascomm <ksascomm@gmail.com>
2020-04-02 10:06:17 -0400

    Tribe Events edits to reflect plugin v5.0

--- library/cleanup.php
+++ library/cleanup.php
@@ -114,3 +114,16 @@ function my_gallery_default_type_set_link( $settings ) {
     return $settings;
 }
 add_filter( 'media_view_settings', 'my_gallery_default_type_set_link');
+
+add_filter( 'tribe_get_event_website_link_label', 'tribe_get_event_website_link_label_default' );
+ 
+//Modern Tribe Events change the text from the URL to a “Visit Website →”
+function tribe_get_event_website_link_label_default( $label ) {
+ 
+    if ( $label === tribe_get_event_website_url() ) {
+        $label = "Visit Website »";
+        return '<a href="' . tribe_get_event_website_url() . '">' . $label . '</a>';
+    }
+ 
+    return $label;
+}
