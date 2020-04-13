2d720fd5a08fea9242d459d265e7c7cd94e1f120
ksascomm <ksascomm@gmail.com>
2020-04-13 11:58:51 -0400

    remove empty <p> from [sidebar-title]Heading Name[/sidebar-title]

--- src/assets/js/lib/accessibility-fixes.js
+++ src/assets/js/lib/accessibility-fixes.js
@@ -3,4 +3,7 @@ jQuery(document).ready( function($) {
 	$('.mobile-off-canvas-menu ul li').removeAttr('role', 'treeitem');
 	$('.mobile-off-canvas-menu ul li').attr('role', 'menuitem');
 	$('.quicklinks ul li').removeAttr('role', 'treeitem');
+	//remove empty <p> from [sidebar-title]Heading Name[/sidebar-title]
+	$('div#custom-sidebar-content p:empty').remove();
 });
+
