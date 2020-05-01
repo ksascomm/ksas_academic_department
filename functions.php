563c783d3614c0b2bbef93ed1a65c14f459a224e
ksascomm <ksascomm@gmail.com>
2020-05-01 09:14:31 -0400

    New favicons

--- functions.php
+++ functions.php
@@ -72,5 +72,8 @@ require_once( 'library/tribe-events.php');
 /** Gutenberg editor support */
 require_once( 'library/gutenberg.php' );
 
+/** Set favicons */
+require_once( 'library/favicons.php' );
+
 /** If your site requires protocol relative url's for theme assets, uncomment the line below */
 // require_once( 'library/class-foundationpress-protocol-relative-theme-assets.php' );
