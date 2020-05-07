1dbfb81aa91d00af9398f50e14c4229cd54c632e
ksascomm <ksascomm@gmail.com>
2020-05-07 15:24:59 -0400

    Remove `tribe_status_fix` patch

--- library/tribe-events.php
+++ library/tribe-events.php
@@ -12,27 +12,3 @@ function tribe_get_event_website_link_label_default( $label ) {
  
     return $label;
 }
-
-add_action('wp_loaded', function() {
-    $query = new WP_Query([
-        'post_type' => 'tribe_events',
-        'post_status' => '-1',
-        'posts_per_page' => -1
-    ]);
-    if ($query->have_posts()) {
-        foreach ($query->posts as $p) {
-            $p->post_status = 'publish';
-            wp_update_post($p);
-        }
-    }
-});
-
-add_filter('tribe_aggregator_before_insert_event', 'tribe_status_fix');
-add_filter('tribe_aggregator_before_save_event', 'tribe_status_fix');
-
-function tribe_status_fix($event) {
-    if (isset($event['post_status']) && $event['post_status'] == -1) {
-        $event['post_status'] = 'publish';
-    }
-    return $event;
-}
