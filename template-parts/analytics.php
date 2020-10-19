be9f48a7c783272767e9f535be2e27ece71e0618
ksascomm <ksascomm@gmail.com>
2020-10-19 15:46:38 -0400

    Update GA to new gtag.js

--- template-parts/analytics.php
+++ template-parts/analytics.php
@@ -1,27 +1,24 @@
-<?php $theme_option = flagship_sub_get_global_options(); 
+<?php $theme_option = flagship_sub_get_global_options();
       $analytics_id = $theme_option['flagship_sub_google_analytics']; ?>
 
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-40512757-1"></script>
 <script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
 
-	ga('create', '<?php echo $analytics_id; ?>', 'jhu.edu');
-	ga('create', 'UA-40512757-1', {'name': 'globalKSAS'});
-	ga('send', 'pageview');
-	ga('globalKSAS.send', 'pageview');
-
+	gtag('config', 'UA-40512757-1');
+	gtag('config', '<?php echo $analytics_id; ?>');
 </script>
 <!-- End Google Analytics -->
-
 <!-- Google Tag Manager -->
 	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 	})(window,document,'script','dataLayer','GTM-5VTN64C');</script>
-<!-- End Google Tag Manager -->		
+<!-- End Google Tag Manager -->
 
 <?php $siteimprove_analytics = $theme_option['flagship_sub_siteimprove_analytics'];
 if ($siteimprove_analytics === 1): ?>
