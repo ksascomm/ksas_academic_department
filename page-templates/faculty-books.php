f35d0bfbc59d5306e311c4a2823d377e6361e634
ksascomm <ksasweb@jhu.edu>
2021-08-23 09:50:03 -0400

    Formatting & Aria-Label fix

--- page-templates/faculty-books.php
+++ page-templates/faculty-books.php
@@ -1,92 +1,101 @@
 <?php
-/*
-Template Name: Faculty Books
-*/
+/**
+ * Template Name: Faculty Books
+ * The template for Faculty Books taxonomy
+ *
+ * @package KSASAcademicDepartment
+ * @since KSASAcademicDepartment 1.0.0
+ */
+
 get_header(); ?>
+
 <div class="main-container" id="page">
-    <div class="main-grid">
-        <main class="main-content">
-            <?php
-            while ( have_posts() ) :
-the_post();
-?>
-                <?php get_template_part( 'template-parts/content', 'page' ); ?>
-            <?php endwhile; ?>
+	<div class="main-grid">
+		<main class="main-content">
+			<?php
+			while ( have_posts() ) :
+				the_post();
+				?>
+				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+			<?php endwhile; ?>
 			<?php
 				$faculty_book_query = new WP_Query(
-                    array(
-						'post_type' => 'post',
-						'category_name' => 'books',
+					array(
+						'post_type'      => 'post',
+						'category_name'  => 'books',
 						'posts_per_page' => 100,
 					)
-                    );
-			 if ( $faculty_book_query->have_posts() ) :
-					 while ($faculty_book_query->have_posts() ) :
+				);
+				if ( $faculty_book_query->have_posts() ) :
+					while ( $faculty_book_query->have_posts() ) :
 						$faculty_book_query->the_post();
 						?>
 						<article class="faculty-book" aria-labelledby="post-<?php the_ID(); ?>">
-						<?php if ( has_post_thumbnail() ) { ?> 
-						<?php
-						the_post_thumbnail(
-							'medium', array(
-								'class'   => 'float-left',
-								'alt' => esc_html ( get_the_title() ),
-							)
-						);
-?>
+						<?php if ( has_post_thumbnail() ) { ?>
+							<?php
+							the_post_thumbnail(
+								'medium',
+								array(
+									'class' => 'float-left',
+									'alt'   => esc_html( get_the_title() ),
+								)
+							);
+							?>
 					<?php } ?>
- <?php
-			   $faculty_post_id = get_post_meta($post->ID, 'ecpt_pub_author', true);
-			   $faculty_post_id2 = get_post_meta($post->ID, 'ecpt_pub_author2', true);
-			   ?>
-			
-					  <ul class="no-bullet">
-						  <li>
-						<h2 itemprop="headline" id="post-<?php the_ID(); ?>">
-					  <?php the_title(); ?>
-						</h2>
-						  </li>
-						  <li>
-						  <?php if ( get_post_meta($post->ID, 'ecpt_pub_date', true) ) : ?> 
-						<?php echo get_post_meta($post->ID, 'ecpt_pub_date', true); ?>,
+						<?php
+							$faculty_post_id  = get_post_meta( $post->ID, 'ecpt_pub_author', true );
+							$faculty_post_id2 = get_post_meta( $post->ID, 'ecpt_pub_author2', true );
+						?>
+					<ul class="no-bullet">
+						<li>
+							<h2 itemprop="headline" id="post-<?php the_ID(); ?>">
+							<?php the_title(); ?>
+							</h2>
+						</li>
+						<li>
+						<?php if ( get_post_meta( $post->ID, 'ecpt_pub_date', true ) ) : ?>
+								<?php echo get_post_meta( $post->ID, 'ecpt_pub_date', true ); ?>,
 					<?php endif; ?>
-						  <?php if ( get_post_meta($post->ID, 'ecpt_publisher', true) ) : ?>
-						<?php echo get_post_meta($post->ID, 'ecpt_publisher', true); ?> 
-					<?php endif; ?>	
-						  </li>
-						  <li>
-						<a href="<?php echo get_permalink($faculty_post_id); ?>">
-					  <?php echo get_the_title($faculty_post_id); ?>, 
-				   <?php if ( get_post_meta($post->ID, 'ecpt_pub_role', true) ) : ?> 
-									<?php echo get_post_meta($post->ID, 'ecpt_pub_role', true); ?>
-								<?php endif; ?>
-						</a>
-						  </li>
-						  <li>
-						  <?php
-							   if (get_post_meta($post->ID, 'ecpt_author_cond', true) == 'on' ) {
-							 ?>
-							 <br>
-							  <a href="<?php echo get_permalink($faculty_post_id2); ?>">
-								<?php echo get_the_title($faculty_post_id2); ?>,&nbsp;
-								<?php echo get_post_meta($post->ID, 'ecpt_pub_role2', true); ?>
-								</a>
-								<?php } ?>
-								   </li>
-								   <li><?php if ( get_post_meta($post->ID, 'ecpt_pub_link', true) ) : ?> 
-								   <a href="http://<?php echo get_post_meta($post->ID, 'ecpt_pub_link', true); ?>" aria-label="Link to purchase <?php the_title(); ?>">
-									Purchase Online <span class="fas fa-external-link-square-alt"></span>
-								   </a>
-								   <?php endif; ?>
-								   </li>
-								   </ul>
-								   <?php the_content(); ?>
-								   <hr>
-								   </article>  
-								   <?php endwhile; endif; ?>      
-        </main>
-        <?php get_sidebar(); ?>
-    </div>
+						<?php if ( get_post_meta( $post->ID, 'ecpt_publisher', true ) ) : ?>
+								<?php echo get_post_meta( $post->ID, 'ecpt_publisher', true ); ?>
+					<?php endif; ?>
+						</li>
+						<li>
+							<a href="<?php echo get_permalink( $faculty_post_id ); ?>">
+							<?php echo get_the_title( $faculty_post_id ); ?>,
+							<?php if ( get_post_meta( $post->ID, 'ecpt_pub_role', true ) ) : ?>
+								<?php echo get_post_meta( $post->ID, 'ecpt_pub_role', true ); ?>
+							<?php endif; ?>
+							</a>
+						</li>
+						<li>
+						<?php
+						if ( get_post_meta( $post->ID, 'ecpt_author_cond', true ) == 'on' ) {
+							?>
+							<br>
+							<a href="<?php echo get_permalink( $faculty_post_id2 ); ?>">
+								<?php echo get_the_title( $faculty_post_id2 ); ?>,&nbsp;
+								<?php echo get_post_meta( $post->ID, 'ecpt_pub_role2', true ); ?>
+							</a>
+						<?php } ?>
+						</li>
+						<li><?php if ( get_post_meta( $post->ID, 'ecpt_pub_link', true ) ) : ?>
+							<a href="http://<?php echo get_post_meta( $post->ID, 'ecpt_pub_link', true ); ?>" aria-label="Purchase Online">
+								Purchase Online <span class="fas fa-external-link-square-alt"></span>
+							</a>
+							<?php endif; ?>
+						</li>
+					</ul>
+						<?php the_content(); ?>
+						<hr>
+						</article>
+						<?php
+						endwhile;
+endif;
+				?>
+		</main>
+		<?php get_sidebar(); ?>
+	</div>
 </div>
 <?php
 get_footer();
--- template-parts/faculty-books.php
+++ template-parts/faculty-books.php
@@ -1,42 +1,43 @@
 <div class="tabs-panel" id="booksTab">
 	<?php
-		$author_id = get_the_ID();
+		$author_id          = get_the_ID();
 		$single_books_query = new WP_Query(
-		array(
-				'post_type' => 'post',
-				'category_name' => 'books',
+			array(
+				'post_type'      => 'post',
+				'category_name'  => 'books',
 				'posts_per_page' => 50,
-				'meta_query' => array(
+				'meta_query'     => array(
 					'relation' => 'OR',
 					array(
-						'key' => 'ecpt_pub_author',
-						'value' => $author_id,
-						'type' => 'NUMERIC',
+						'key'     => 'ecpt_pub_author',
+						'value'   => $author_id,
+						'type'    => 'NUMERIC',
 						'compare' => '=',
 					),
 					array(
-						'key' => 'ecpt_pub_author2',
-						'value' => $author_id,
-						'type' => 'NUMERIC',
+						'key'     => 'ecpt_pub_author2',
+						'value'   => $author_id,
+						'type'    => 'NUMERIC',
 						'compare' => '=',
 					),
 				),
 			)
-	);
+		);
 		if ( $single_books_query->have_posts() ) :
-	while ($single_books_query->have_posts() ) :
-	$single_books_query->the_post(); ?>
+			while ( $single_books_query->have_posts() ) :
+				$single_books_query->the_post(); ?>
 	<article class="book-entry" aria-labelledby="post-<?php the_ID(); ?>">
-		<?php
-		if ( has_post_thumbnail() ) {
-			the_post_thumbnail(
-				'directory', array(
-					'class'   => 'float-left',
-					'alt' => esc_html ( get_the_title() ),
-				)
-			); 
-		}
-		?>
+				<?php
+				if ( has_post_thumbnail() ) {
+					the_post_thumbnail(
+						'directory',
+						array(
+							'class' => 'float-left',
+							'alt'   => esc_html( get_the_title() ),
+						)
+					);
+				}
+				?>
 		<ul class="no-bullet">
 			<li>
 				<h5>
@@ -46,27 +47,28 @@
 				</h5>
 			</li>
 			<li>
-				<?php if ( get_post_meta($post->ID, 'ecpt_pub_date', true) ) : ?> 
-					<?php echo get_post_meta($post->ID, 'ecpt_pub_date', true); ?>,
+				<?php if ( get_post_meta( $post->ID, 'ecpt_pub_date', true ) ) : ?>
+					<?php echo get_post_meta( $post->ID, 'ecpt_pub_date', true ); ?>,
 				<?php endif; ?>
-				<?php if ( get_post_meta($post->ID, 'ecpt_publisher', true) ) : ?>
-					<?php echo get_post_meta($post->ID, 'ecpt_publisher', true); ?> 
-				<?php endif; ?>	
+				<?php if ( get_post_meta( $post->ID, 'ecpt_publisher', true ) ) : ?>
+					<?php echo get_post_meta( $post->ID, 'ecpt_publisher', true ); ?>
+				<?php endif; ?>
 			</li>
 			<li>
-				<strong>Role:</strong> <span class="capitalize"><?php echo get_post_meta($post->ID, 'ecpt_pub_role', true); ?></span>
+				<strong>Role:</strong> <span class="capitalize"><?php echo get_post_meta( $post->ID, 'ecpt_pub_role', true ); ?></span>
 			</li>
 			<li>
 				<?php
-				if (get_post_meta($post->ID, 'ecpt_author_cond', true) == 'on' ) {
-					$faculty_post_id2 = get_post_meta($post->ID, 'ecpt_pub_author2', true); ?>
+				if ( get_post_meta( $post->ID, 'ecpt_author_cond', true ) == 'on' ) {
+					$faculty_post_id2 = get_post_meta( $post->ID, 'ecpt_pub_author2', true );
+					?>
 					<br>
-					<?php echo get_the_title($faculty_post_id2); ?>,&nbsp;<?php echo get_post_meta($post->ID, 'ecpt_pub_role2', true); ?>
+					<?php echo get_the_title( $faculty_post_id2 ); ?>,&nbsp;<?php echo get_post_meta( $post->ID, 'ecpt_pub_role2', true ); ?>
 				<?php } ?>
 			</li>
 			<li>
-				<?php if ( get_post_meta($post->ID, 'ecpt_pub_link', true) ) : ?>
-					<a href="http://<?php echo get_post_meta($post->ID, 'ecpt_pub_link', true); ?>" aria-label="Link to purchase <?php the_title(); ?>">
+				<?php if ( get_post_meta( $post->ID, 'ecpt_pub_link', true ) ) : ?>
+					<a href="http://<?php echo get_post_meta( $post->ID, 'ecpt_pub_link', true ); ?>" aria-label="Purcase Online">
 						Purchase Online <span class="fas fa-external-link-square-alt"></span>
 					</a>
 				<?php endif; ?>
@@ -74,5 +76,9 @@
 		</ul>
 		<hr>
 	</article>
-	<?php endwhile; endif; wp_reset_query(); ?>
+				<?php
+			endwhile;
+endif;
+		wp_reset_postdata()
+		?>
 </div>
