ade790340da198c9756c8ef7ef0fa774d40dbf55
ksascomm <ksasweb@jhu.edu>
2021-09-08 09:26:25 -0400

    Match visual label and accessible name

--- template-parts/content-books.php
+++ template-parts/content-books.php
@@ -10,43 +10,44 @@
 
 ?>
 <?php
-$faculty_post_id = get_post_meta($post->ID, 'ecpt_pub_author', true);
-		  $faculty_post_id2 = get_post_meta($post->ID, 'ecpt_pub_author2', true);
-          ?>
+$faculty_post_id  = get_post_meta( $post->ID, 'ecpt_pub_author', true );
+$faculty_post_id2 = get_post_meta( $post->ID, 'ecpt_pub_author2', true );
+?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class('post-singular'); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'post-singular' ); ?>>
 		<header>
 			<h1 class="entry-title"><?php the_title(); ?></h1>
 		</header>
 		<div class="entry-content single-book">
-				<?php $title=get_the_title();
-					the_post_thumbnail('medium', array( 'alt' => $title ) );
+				<?php
+				$title = get_the_title();
+					the_post_thumbnail( 'medium', array( 'alt' => $title ) );
 				?>
 				<ul class="no-bullet">
 					<li>
-					<?php if ( get_post_meta($post->ID, 'ecpt_pub_date', true) ) : ?>
-						<?php echo get_post_meta($post->ID, 'ecpt_pub_date', true); ?>,
+					<?php if ( get_post_meta( $post->ID, 'ecpt_pub_date', true ) ) : ?>
+						<?php echo get_post_meta( $post->ID, 'ecpt_pub_date', true ); ?>,
 					<?php endif; ?>
-					<?php if ( get_post_meta($post->ID, 'ecpt_publisher', true) ) : ?>
-						<?php echo get_post_meta($post->ID, 'ecpt_publisher', true); ?>
+					<?php if ( get_post_meta( $post->ID, 'ecpt_publisher', true ) ) : ?>
+						<?php echo get_post_meta( $post->ID, 'ecpt_publisher', true ); ?>
 					<?php endif; ?>
 					</li>
 					<li>
-						<a href="<?php echo get_permalink($faculty_post_id); ?>">
-							<?php echo get_the_title($faculty_post_id); ?>,
-							<?php if ( get_post_meta($post->ID, 'ecpt_pub_role', true) ) : ?>
-								<?php echo get_post_meta($post->ID, 'ecpt_pub_role', true); ?>
+						<a href="<?php echo get_permalink( $faculty_post_id ); ?>">
+							<?php echo get_the_title( $faculty_post_id ); ?>,
+							<?php if ( get_post_meta( $post->ID, 'ecpt_pub_role', true ) ) : ?>
+								<?php echo get_post_meta( $post->ID, 'ecpt_pub_role', true ); ?>
 							<?php endif; ?>
 						</a>
 					</li>
-					<?php if (get_post_meta($post->ID, 'ecpt_author_cond', true) == 'on' ) { ?>
+					<?php if ( get_post_meta( $post->ID, 'ecpt_author_cond', true ) == 'on' ) { ?>
 					<li>
-						<a href="<?php echo get_permalink($faculty_post_id2); ?>"><?php echo get_the_title($faculty_post_id2); ?>,&nbsp;<?php echo get_post_meta($post->ID, 'ecpt_pub_role2', true); ?>
+						<a href="<?php echo get_permalink( $faculty_post_id2 ); ?>"><?php echo get_the_title( $faculty_post_id2 ); ?>,&nbsp;<?php echo get_post_meta( $post->ID, 'ecpt_pub_role2', true ); ?>
 						</a>
 					</li>
 					<?php } ?>
-					<li><?php if ( get_post_meta($post->ID, 'ecpt_pub_link', true) ) : ?>
-							<a href="http://<?php echo get_post_meta($post->ID, 'ecpt_pub_link', true); ?>" aria-label="Link to purchase <?php the_title(); ?>">
+					<li><?php if ( get_post_meta( $post->ID, 'ecpt_pub_link', true ) ) : ?>
+							<a href="http://<?php echo get_post_meta( $post->ID, 'ecpt_pub_link', true ); ?>" aria-label="Purchase Online">
 								Purchase Online <span class="fas fa-external-link-square-alt"></span>
 							</a>
 						<?php endif; ?>
