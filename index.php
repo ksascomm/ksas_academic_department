8727be53e4e1fc621d1556e4a2b1fc3a17e69234
ksascomm <ksasweb@jhu.edu>
2021-09-27 11:37:59 -0400

    Formatting tweaks

--- index.php
+++ index.php
@@ -14,38 +14,45 @@
  */
 
 get_header();
-	$theme_option = flagship_sub_get_global_options();
-	$collection_name = $theme_option['flagship_sub_search_collection'];
+	$theme_option    = flagship_sub_get_global_options();
 	$news_query_cond = $theme_option['flagship_sub_news_query_cond']; ?>
 	<div class="main-container" id="page">
 		<div class="main-grid">
 			<main class="main-content">
 				<h1 class="page-title"><?php echo $theme_option['flagship_sub_feed_name']; ?> Archive</h1>
 
-			<?php $paged = (get_query_var('paged')) ? (int) get_query_var('paged') : 1;
+			<?php
+			$paged = ( get_query_var( 'paged' ) ) ? (int) get_query_var( 'paged' ) : 1;
 
-				if ($news_query_cond === 1) {
-					$news_archive_query = new WP_Query(array(
-						'post_type' => array('post'),
-						'tax_query' => array(
+			if ( $news_query_cond === 1 ) {
+				$news_archive_query = new WP_Query(
+					array(
+						'post_type'      => array( 'post' ),
+						'tax_query'      => array(
 							array(
 								'taxonomy' => 'category',
-								'field' => 'slug',
-								'terms' => array( 'books' ),
-								'operator' => 'NOT IN'
-							)
+								'field'    => 'slug',
+								'terms'    => array( 'books' ),
+								'operator' => 'NOT IN',
+							),
 						),
 						'posts_per_page' => 10,
-						'paged' => $paged));
-				} else {
-					$news_archive_query = new WP_Query(array(
-						'post_type' => array('post'),
+						'paged'          => $paged,
+					)
+				);
+			} else {
+				$news_archive_query = new WP_Query(
+					array(
+						'post_type'      => array( 'post' ),
 						'posts_per_page' => 10,
-						'paged' => $paged
-						));
-				}
+						'paged'          => $paged,
+					)
+				);
+			}
 
-			while ($news_archive_query->have_posts()) : $news_archive_query->the_post(); ?>
+			while ( $news_archive_query->have_posts() ) :
+				$news_archive_query->the_post();
+				?>
 
 					<?php get_template_part( 'template-parts/content-news', get_post_format() ); ?>
 				<?php endwhile; ?>
@@ -56,7 +63,7 @@ get_header();
 			if ( function_exists( 'ksasacademic_pagination' ) ) :
 				ksasacademic_pagination();
 			elseif ( is_paged() ) :
-			?>
+				?>
 				<nav id="post-nav">
 					<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'ksasacademic' ) ); ?></div>
 					<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'ksasacademic' ) ); ?></div>
