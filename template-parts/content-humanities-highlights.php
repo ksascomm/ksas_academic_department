49a9edd62b539f481a2b485d0949d7009fcebdf6
ksascomm <ksasweb@jhu.edu>
2021-10-18 10:01:39 -0400

    Add pre-existing ACF conditional

--- template-parts/content-humanities-highlights.php
+++ template-parts/content-humanities-highlights.php
@@ -10,35 +10,71 @@
 
 ?>
 
-<article aria-labelledby="post-<?php the_ID(); ?>" <?php post_class('post-listing news-article grid-x grid-margin-x'); ?>>
+<article aria-labelledby="post-<?php the_ID(); ?>" <?php post_class( 'post-listing news-article grid-x grid-margin-x' ); ?>>
 
-<?php if( has_tag( 'video' ) ) : ?>
+<?php if ( has_tag( 'video' ) ) : ?>
 	<div class="large-6 cell">
 		<?php the_content(); ?>
 	</div>
 	<div class="large-6 cell">
 		<header>
 			<h1 id="post-<?php the_ID(); ?>">
-				<a href="<?php if ( get_post_meta($post->ID, 'ecpt_highlight_location', true) ) : ?><?php echo get_post_meta($post->ID, 'ecpt_highlight_location', true); ?><?php else : ?><?php the_permalink();?><?php endif ;?>"><?php the_title();?></a>
+			<a href="
+				<?php
+				if ( get_post_meta( $post->ID, 'ecpt_highlight_location', true ) ) :
+					?>
+					<?php echo get_post_meta( $post->ID, 'ecpt_highlight_location', true ); ?>
+					<?php
+				elseif ( get_post_meta( $post->ID, 'ecpt_external_link', true ) ) :
+						echo get_post_meta( $post->ID, 'ecpt_external_link', true );
+				else :
+						the_permalink();
+				endif;
+				?>
+				">
+					<?php the_title(); ?>
+				</a>
 			</h1>
 		</header>
 		<div class="entry-content">
 			<?php the_excerpt(); ?>
 		</div>
 	</div>
-<?php else: ?>
+<?php else : ?>
 	<div class="large-4 cell">
-		<?php the_post_thumbnail('large', array('class'	=> "front-humanities-highlight", 'alt' => esc_html ( get_the_title() ))); ?>
+		<?php
+		the_post_thumbnail(
+			'large',
+			array(
+				'class' => 'front-humanities-highlight',
+				'alt'   => esc_html( get_the_title() ),
+			)
+		);
+		?>
 	</div>
 	<div class="large-8 cell">
 		<header>
 			<h1 id="post-<?php the_ID(); ?>">
-				<a href="<?php if ( get_post_meta($post->ID, 'ecpt_highlight_location', true) ) : ?><?php echo get_post_meta($post->ID, 'ecpt_highlight_location', true); ?><?php else : ?><?php the_permalink();?><?php endif ;?>"><?php the_title();?></a>
+			<a href="
+				<?php
+				if ( get_post_meta( $post->ID, 'ecpt_highlight_location', true ) ) :
+					?>
+					<?php echo get_post_meta( $post->ID, 'ecpt_highlight_location', true ); ?>
+					<?php
+				elseif ( get_post_meta( $post->ID, 'ecpt_external_link', true ) ) :
+						echo get_post_meta( $post->ID, 'ecpt_external_link', true );
+				else :
+						the_permalink();
+				endif;
+				?>
+				">
+					<?php the_title(); ?>
+				</a>
 			</h1>
 		</header>
 		<div class="entry-content">
 			<?php the_content(); ?>
 		</div>
 	</div>
-<?php endif;?>
+<?php endif; ?>
 </article>
