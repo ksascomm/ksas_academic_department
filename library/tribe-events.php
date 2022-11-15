7d54cc2679659c3e61529882e809e8a59f741d16
ksascomm <ksasweb@jhu.edu>
2022-11-15 12:53:30 -0500

    Event categories in list view conditional

--- library/tribe-events.php
+++ library/tribe-events.php
@@ -24,10 +24,13 @@ add_action(
 	'tribe_template_before_include:events/v2/list/event/venue',
 	function() {
 		global $post;
+		$event_categories = tribe_get_event_taxonomy( $post->ID );
 		?>
-	<ul class='tribe-event-categories'>
-		<?php echo tribe_get_event_taxonomy( $post->ID ); ?>
-	</ul>
+		<?php if ( ! empty( $event_categories ) ) : ?>
+			<ul class='tribe-event-categories'>
+				<?php echo tribe_get_event_taxonomy( $post->ID ); ?>
+			</ul>
+		<?php endif; ?>
 		<?php
 	}
 );
