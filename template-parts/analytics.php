84d86f69c7c16c47307ef6e5151af1e0a6c53c1c
ksascomm <ksasweb@jhu.edu>
2023-08-21 12:08:23 -0400

    Remove UA script

--- template-parts/analytics.php
+++ template-parts/analytics.php
@@ -7,30 +7,6 @@
  */
 
 ?>
-
-<?php
-$theme_option = flagship_sub_get_global_options();
-$analytics_id = $theme_option['flagship_sub_google_analytics'];
-
-?>
-
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-40512757-1"></script>
-<script>
-	window.dataLayer = window.dataLayer || [];
-	function gtag(){dataLayer.push(arguments);}
-	gtag('js', new Date());
-
-	gtag('config', 'UA-40512757-1');
-	gtag('config', '<?php echo $analytics_id; ?>');
-	<?php
-	if ( isset( $theme_option['flagship_sub_ga4_analytics'] ) ) :
-		?>
-		gtag('config', '<?php echo $theme_option['flagship_sub_ga4_analytics']; ?>');
-	<?php endif; ?>
-</script>
-<!-- End Google Analytics -->
-
 <!-- Google Tag Manager -->
 	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
