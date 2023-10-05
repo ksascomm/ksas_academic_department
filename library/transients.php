3d4cac9bab22da0524291f57b3ca3ac17c5b0324
ksascomm <ksasweb@jhu.edu>
2023-10-05 09:29:25 -0400

    Re-add transient clearing functions

--- library/transients.php
+++ library/transients.php
@@ -1,27 +1,24 @@
 <?php
-function delete_academic_transients( $post_id ) {
-	global $post;
-	if ( isset( $_GET['post_type'] ) ) {
-		$post_type = $_GET['post_type'];
-	} else {
-		$post_type = $post->post_type;
-	}
-	switch ( $post_type ) {
-		case 'people':
-			$roles = get_terms(
-				'role',
-				array(
-					'orderby'    => 'id',
-					'hide_empty' => true,
-				)
-			);
-			foreach ( $roles as $role ) {
-				$role_slug = $role->slug;
-				delete_transient( 'job_market_query' );
-				delete_transient( 'research_staff_query' );
-				delete_transient( 'graduate_student_query' );
-			}
-			break;
-	}
+/**
+ * Create simple function to delete our People CPT transients
+ */
+
+add_action( 'save_post_people', 'delete_academic_transients_save' );
+	/**
+	 * Check status here or check if post has been created.
+	 */
+function delete_academic_transients_save() {
+	delete_transient( 'job_market_query' );
+	delete_transient( 'research_staff_query' );
+	delete_transient( 'graduate_student_query' );
 }
-add_action( 'save_post', 'delete_academic_transients' );
+
+add_action( 'edit_post_people', 'delete_academic_transients_edit' );
+	/**
+	 * Check status here or check if post data has been edited.
+	 */
+function delete_academic_transients_edit() {
+	delete_transient( 'job_market_query' );
+	delete_transient( 'research_staff_query' );
+	delete_transient( 'graduate_student_query' );
+}
