215be6f229625162cfb7ef9fca75b2c02a8ad11f
ksascomm <ksasweb@jhu.edu>
2024-04-05 07:02:20 -0400

    Add Summer 2024 courses

--- page-templates/courses-undergrad-all.php
+++ page-templates/courses-undergrad-all.php
@@ -19,7 +19,7 @@ get_header(); ?>
 	$department         = str_replace( ' ', '%20', $department_unclean );
 	$department         = str_replace( '&', '%26', $department );
 	$fall               = 'fall%202024';
-	$summer             = 'summer%202023';
+	$summer             = 'summer%202024';
 	$spring             = 'spring%202024';
 	$open               = 'open';
 	$approval           = 'approval%20required';
